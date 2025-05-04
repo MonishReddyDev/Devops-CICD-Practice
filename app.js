f76e4aa047d0e9c4ea16a4b3b7c59d816c17df2a
monish <monishreddy9991@gmail.com>
2025-05-04 15:03:29 -0500

    Change home page message - Feature 1

--- app.js
+++ app.js
@@ -14,6 +14,10 @@ app.get('/about', (req, res) => {
     res.send('About Page - Todo App');
 });
 
+// Home route changes
+app.get('/new', (req, res) => {
+    res.send('Hello from Feature Branch 1!');
+});
 
 // Home route
 app.get('/', (req, res) => {
